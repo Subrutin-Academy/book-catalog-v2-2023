7e61297e2d5ef5f73d23a6d5cf875bb5b03b108a
Tedy Tri Saputro <detik19@gmail.com>
2022-12-21 21:01:02 +0700

    [ADD] add env properties

--- src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
@@ -5,20 +5,25 @@ import java.util.TimeZone;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.config.ApplicationProperties;
+import com.subrutin.catalog.config.CloudProperties;
 import com.subrutin.catalog.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
 	private ApplicationProperties appProperties;
+	
+	private CloudProperties cloudProperties;
 
-	public GreetingServiceImpl(ApplicationProperties appProperties) {
+	public GreetingServiceImpl(ApplicationProperties appProperties, CloudProperties cloudProperties) {
 		super();
 		this.appProperties = appProperties;
+		this.cloudProperties = cloudProperties;
 	}
 
 	@Override
 	public String sayGreeting() {
+		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText() + ", our timezone :" + timezone.getDisplayName() + ", our currency:"
 				+ appProperties.getCurrency();
