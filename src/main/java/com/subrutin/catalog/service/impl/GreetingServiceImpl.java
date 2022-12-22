2f99f1a4d3001c119424eca7c2a8f790ff91b82d
Tedy Tri Saputro <detik19@gmail.com>
2022-12-22 09:02:53 +0700

    [ADD] add logging

--- src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
@@ -2,6 +2,8 @@ package com.subrutin.catalog.service.impl;
 
 import java.util.TimeZone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.config.ApplicationProperties;
@@ -10,6 +12,8 @@ import com.subrutin.catalog.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
+	Logger log = LoggerFactory.getLogger(GreetingServiceImpl.class);
+
 
 	private ApplicationProperties appProperties;
 	
@@ -23,6 +27,11 @@ public class GreetingServiceImpl implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
+		log.trace("this is log TRACE");
+		log.debug("this is log DEBUG");
+		log.info("this is log INFO");
+		log.warn("this is log WARN");
+		log.error("this is log ERROR");
 		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText() + ", our timezone :" + timezone.getDisplayName() + ", our currency:"
