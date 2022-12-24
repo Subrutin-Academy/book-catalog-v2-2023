50ad8641135313320aff404ffcc8de2ca9e89c25
Tedy Tri Saputro <detik19@gmail.com>
2022-12-24 11:01:46 +0700

    add slf4j

--- src/main/java/com/subrutin/catalog/web/BookResource.java
+++ src/main/java/com/subrutin/catalog/web/BookResource.java
@@ -1,5 +1,6 @@
 package com.subrutin.catalog.web;
 
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,24 @@ import com.subrutin.catalog.dto.BookDetailDTO;
 import com.subrutin.catalog.service.BookService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 public class BookResource {
-
-    private final BookService bookService;
     
+    private final BookService bookService;
+
+
+
     @GetMapping("/book/{bookId}")
     public BookDetailDTO findBookDetail(@PathVariable("bookId") Long id) {
-        return bookService.findBookDetailById(id);
+        StopWatch stopWatch = new StopWatch();
+        log.info("start findBookDetail "+id);
+        BookDetailDTO result =  bookService.findBookDetailById(id);
+        log.info("finish findBookDetail. execution time = {}",stopWatch);
+        return result;
+
     }
 }
