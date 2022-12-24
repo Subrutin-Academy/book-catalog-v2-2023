9c0dfe0b8e4e0f786bfe0769cc72882dd719a29b
Tedy Tri Saputro <detik19@gmail.com>
2022-12-24 11:54:31 +0700

    [ADD] add book new

--- src/main/java/com/subrutin/catalog/controller/BookController.java
+++ src/main/java/com/subrutin/catalog/controller/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import com.subrutin.catalog.dto.BookCreateDTO;
 import com.subrutin.catalog.dto.BookDetailDTO;
 import com.subrutin.catalog.service.BookService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -36,7 +39,14 @@ public class BookController {
 	}
 
 	@PostMapping("/new")
-	public String addNewBook(@ModelAttribute("bookCreateDTO") BookCreateDTO dto, Model model) {
+	public String addNewBook(@ModelAttribute("bookCreateDTO") @Valid BookCreateDTO dto, 
+			BindingResult bindingResult,
+			Errors errors,
+			Model model) {
+		if(errors.hasErrors()) {
+			model.addAttribute("bookCreateDTO", dto);
+			return "book/book-new";
+		}
 		bookService.createNewBook(dto);
 		return "redirect:/book/list";
 
