fef9cb259a66ce2c1d4fec3401f27f3226885270
Tedy Tri Saputro <detik19@gmail.com>
2022-12-24 11:22:15 +0700

    [ADD] add thymeleaf

---
+++ src/main/java/com/subrutin/catalog/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.subrutin.catalog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/home")
+//    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        return "home";
+    }
+
+}
