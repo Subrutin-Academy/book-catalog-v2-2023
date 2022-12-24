203ee990098352811a0c915de5bd000b2ceccb88
Tedy Tri Saputro <detik19@gmail.com>
2022-12-24 21:44:21 +0700

    [ADD} add validation

--- src/main/java/com/subrutin/catalog/dto/BookCreateDTO.java
+++ src/main/java/com/subrutin/catalog/dto/BookCreateDTO.java
@@ -1,12 +1,15 @@
 package com.subrutin.catalog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class BookCreateDTO {
 	
+	@NotBlank
 	private String bookTitle;
 	
+	@NotBlank
 	private String authorName;
 	
 	private String description;
