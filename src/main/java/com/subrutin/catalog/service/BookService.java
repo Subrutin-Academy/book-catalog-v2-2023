e535c49dc7132c33553cb996bd41e0ffc460ec48
Tedy Tri Saputro <detik19@gmail.com>
2022-12-24 10:50:17 +0700

    [ADD] DI and lombok

---
+++ src/main/java/com/subrutin/catalog/service/BookService.java
@@ -0,0 +1,7 @@
+package com.subrutin.catalog.service;
+
+import com.subrutin.catalog.dto.BookDetailDTO;
+
+public interface BookService {
+    public BookDetailDTO findBookDetailById(Long bookId);
+}
---
+++ src/main/java/com/subrutin/catalog/service/impl/BookServiceImpl.java
@@ -0,0 +1,30 @@
+package com.subrutin.catalog.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.subrutin.catalog.domain.Book;
+import com.subrutin.catalog.dto.BookDetailDTO;
+import com.subrutin.catalog.repository.BookRepository;
+import com.subrutin.catalog.service.BookService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service("bookService")
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+
+    @Override
+    public BookDetailDTO findBookDetailById(Long bookId) {
+        Book book = bookRepository.findBookById(bookId);
+        BookDetailDTO dto = new BookDetailDTO();
+        dto.setBookId(book.getId());
+        dto.setAuthorName(book.getAuthor().getName());
+        dto.setBookTitle(book.getTitle());
+        dto.setBookDescription(book.getDescription());
+        return dto;
+    }
+
+}
