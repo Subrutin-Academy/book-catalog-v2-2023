50b1ca87f70ea2a75d685fb4d9ca45f76557c8ec
Tedy Tri Saputro <detik19@gmail.com>
2022-12-25 17:12:20 +0700

    [ADD] dynamic update

--- src/main/java/com/subrutin/catalog/domain/Author.java
+++ src/main/java/com/subrutin/catalog/domain/Author.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+//@DynamicUpdate
 @Entity(name="author")
 public class Author {
 	
--- src/main/java/com/subrutin/catalog/web/AuthorResource.java
+++ src/main/java/com/subrutin/catalog/web/AuthorResource.java
@@ -45,4 +45,5 @@ public class AuthorResource {
 		return ResponseEntity.ok().build();
 	}
 
+
 }
