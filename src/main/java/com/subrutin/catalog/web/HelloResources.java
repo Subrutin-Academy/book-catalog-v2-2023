011c712e02c29a48353f913788e6ebdded96bf5b
Tedy Tri Saputro <detik19@gmail.com>
2022-12-25 09:55:00 +0700

    [ADD} entity

--- src/main/java/com/subrutin/catalog/web/HelloResources.java
+++ src/main/java/com/subrutin/catalog/web/HelloResources.java
@@ -1,24 +1,36 @@
 package com.subrutin.catalog.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.subrutin.catalog.dto.HelloMessageResponseDTO;
 import com.subrutin.catalog.service.GreetingService;
 
 @RestController
 public class HelloResources {
 
-private GreetingService greetingService;
-	
+	private GreetingService greetingService;
+
+	Logger log = LoggerFactory.getLogger(HelloResources.class);
+
 	public HelloResources(GreetingService greetingService) {
 		super();
 		this.greetingService = greetingService;
 	}
 
-
-
+	// get adalah salah satu jenis dari http method/ http verb
+	// POST, PUT, DELETE, OPTION, TRACE, HEAD, PATCH
 	@GetMapping("/hello")
-	public String helloWorld() {
-		return greetingService.sayGreeting();
+	public HelloMessageResponseDTO helloWorld() {
+		log.trace("this is log TRACE");
+		log.debug("this is log DEBUG");
+		log.info("this is log INFO");
+		log.warn("this is log WARN");
+		log.error("this is log ERROR");
+		HelloMessageResponseDTO dto = new HelloMessageResponseDTO();
+		dto.setMessage(greetingService.sayGreeting());
+		return dto;
 	}
 }
