4008f4bd007455fc69559b3c397fdd0650a6530a
Tedy Tri Saputro <detik19@gmail.com>
2022-12-25 18:29:26 +0700

    [ADD] add many to many

--- src/main/java/com/subrutin/catalog/domain/Book.java
+++ src/main/java/com/subrutin/catalog/domain/Book.java
@@ -1,6 +1,7 @@
 package com.subrutin.catalog.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -36,7 +39,19 @@ public class Book implements Serializable {
 	@JoinColumn(name = "publisher_id", nullable = false)
 	private Publisher publisher;
 	
+	@ManyToMany
+	@JoinTable(name = "book_author", joinColumns = {
+			@JoinColumn(name="book_id", referencedColumnName = "id")},
+	inverseJoinColumns = {
+			@JoinColumn(name="author_id",referencedColumnName = "id")
+	})
+	private List<Author> authors;
 	
-	
-
+	@ManyToMany
+	@JoinTable(name = "book_category", joinColumns = {
+			@JoinColumn(name="book_id", referencedColumnName = "id")},
+	inverseJoinColumns = {
+			@JoinColumn(name="category_code", referencedColumnName = "code")
+	})
+	private List<Category> categories;
 }
