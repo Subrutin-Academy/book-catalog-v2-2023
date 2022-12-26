b7b8ee7e0dfe40f797fd0a6f9d2e4b1b4d90fef7
Tedy Tri Saputro <detik19@gmail.com>
2022-12-26 08:59:04 +0700

    add logging annotation

--- src/main/java/com/subrutin/catalog/aspect/LoggingAspect.java
+++ src/main/java/com/subrutin/catalog/aspect/LoggingAspect.java
@@ -1,5 +1,8 @@
 package com.subrutin.catalog.aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -24,9 +27,29 @@ public class LoggingAspect {
 	@Pointcut("@args(com.subrutin.catalog.annotation.LogThisArg)")
 	private void argsAnnotationPointcutExample() {}
 	
-	@Before("restAPI() && argsAnnotationPointcutExample()")
+	@Pointcut("@annotation(com.subrutin.catalog.annotation.LogThisMethod)")
+	private void annotationPointcutExample() {}
+	
+	@Before("annotationPointcutExample()")
 	public void beforeExecutedLogging() {
-		log.info("this is log from aspect");
+		log.info("this is log from aspect before method executed");
+	}
+	
+	@After("annotationPointcutExample()")
+	public void afterExecutedLogging() {
+		log.info("this is log from aspect after method executed");
+	}
+
+	
+	@AfterReturning("annotationPointcutExample()")
+	public void afterReturnExecutedLogging() {
+		log.info("this is log from aspect after returning method executed");
+	}
+	
+	
+	@AfterThrowing("annotationPointcutExample()")
+	public void afterThrowingExecutedLogging() {
+		log.info("this is log from aspect after throwing method executed");
 	}
 
 }
