e7c615b8376208b48c0384faaa4ebc461250a77d
Tedy Tri Saputro <detik19@gmail.com>
2022-12-26 20:10:47 +0700

    [ADD] add aop exception

--- src/main/java/com/subrutin/catalog/enums/ErrorCode.java
+++ src/main/java/com/subrutin/catalog/enums/ErrorCode.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ErrorCode {
 
-	INVALID_DATA(1),INTERNAL_ERROR(2), NETWORK_ERROR(3), OTHER_ERROR(4);
+	INVALID_DATA(1),INTERNAL_ERROR(2), NETWORK_ERROR(3), OTHER_ERROR(4), DATA_NOT_FOUND(5);
 
 	private int code;
 	ErrorCode(int code) {
