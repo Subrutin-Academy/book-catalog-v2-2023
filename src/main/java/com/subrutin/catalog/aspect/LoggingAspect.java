ba6126fd4235cd652d5b4df443915e80f2e7cc0c
Tedy Tri Saputro <detik19@gmail.com>
2022-12-26 08:51:49 +0700

    [ADD] add pcd args

--- src/main/java/com/subrutin/catalog/aspect/LoggingAspect.java
+++ src/main/java/com/subrutin/catalog/aspect/LoggingAspect.java
@@ -12,13 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 public class LoggingAspect {
 	
-	@Pointcut("execution(* com.subrutin.catalog.web.*.*(..))")
+	@Pointcut("execution(* com.subrutin.catalog.web.*.*(com.subrutin.catalog.dto.PublisherCreateRequestDTO))")
 	private void restAPI() {}
 	
 	@Pointcut("within(com.subrutin.catalog.web.*)")
 	private void withinPointcutExample() {}
 	
-	@Before("withinPointcutExample()")
+	@Pointcut("args(com.subrutin.catalog.dto.PublisherCreateRequestDTO)")
+	private void argsPointcutExample() {}
+	
+	@Before("restAPI()")
 	public void beforeExecutedLogging() {
 		log.info("this is log from aspect");
 	}
