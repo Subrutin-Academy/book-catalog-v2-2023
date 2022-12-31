12f3a93b7f85e08a5fb9a200772ad4a197688939
Tedy Tri Saputro <detik19@gmail.com>
2022-12-31 22:14:33 +0700

    update java 17

--- src/main/java/com/subrutin/catalog/domain/Author.java
+++ src/main/java/com/subrutin/catalog/domain/Author.java
@@ -1,15 +1,18 @@
 package com.subrutin.catalog.domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
