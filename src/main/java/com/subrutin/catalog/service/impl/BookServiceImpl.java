5a362f670d42b96c711cfd4064b065c5f5b3cc07
Tedy Tri Saputro <detik19@gmail.com>
2022-12-31 22:36:29 +0700

    [ADD] add jpql

--- src/main/java/com/subrutin/catalog/service/impl/BookServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/BookServiceImpl.java
@@ -3,6 +3,13 @@ package com.subrutin.catalog.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.domain.Author;
@@ -11,15 +18,20 @@ import com.subrutin.catalog.domain.Category;
 import com.subrutin.catalog.domain.Publisher;
 import com.subrutin.catalog.dto.BookCreateRequestDTO;
 import com.subrutin.catalog.dto.BookDetailResponseDTO;
+import com.subrutin.catalog.dto.BookListResponseDTO;
 import com.subrutin.catalog.dto.BookUpdateRequestDTO;
+import com.subrutin.catalog.dto.ResultPageResponseDTO;
 import com.subrutin.catalog.exception.BadRequestException;
+import com.subrutin.catalog.repository.AuthorRepository;
 import com.subrutin.catalog.repository.BookRepository;
 import com.subrutin.catalog.service.AuthorService;
 import com.subrutin.catalog.service.BookService;
 import com.subrutin.catalog.service.CategoryService;
 import com.subrutin.catalog.service.PublisherService;
+import com.subrutin.catalog.util.PaginationUtil;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -114,4 +126,35 @@ public class BookServiceImpl implements BookService {
 		
 	}
 
+
+	@Override
+	public ResultPageResponseDTO<BookListResponseDTO> findBookList(Integer page, Integer limit, String sortBy,
+			String direction, String publisherName, String bookTitle,String authorName) {
+		Sort sort = Sort.by(new Sort.Order(PaginationUtil.getSortBy(direction),sortBy));
+		Pageable pageable = PageRequest.of(page, limit, sort);
+		Page<Book> pageResult = bookRepository.findBookList(bookTitle, publisherName,authorName, pageable);
+		List<BookListResponseDTO> dtos = pageResult.stream().map(b->{
+			BookListResponseDTO dto =  new BookListResponseDTO();
+			dto.setAuthorNames(b.getAuthors().stream().map(a->a.getName()).collect(Collectors.toList()));
+			dto.setCategoryCodes(b.getCategories().stream().map(c->c.getCode()).collect(Collectors.toList()));
+			dto.setTitle(b.getTitle());
+			dto.setPublisherName(b.getPublisher().getName());
+			dto.setDescription(b.getDescription());
+			dto.setId(b.getSecureId());
+			return dto;
+		}).collect(Collectors.toList());
+		return PaginationUtil.createResultPageDTO(dtos, pageResult.getTotalElements(), pageResult.getTotalPages());
+	}
+
+//	public BookRepository getBookRepository() {
+//		return bookRepository;
+//	}
+//
+//	public void setBookRepository(BookRepository bookRepository) {
+//		this.bookRepository = bookRepository;
+//	}
+	
+	
+	
+	
 }
