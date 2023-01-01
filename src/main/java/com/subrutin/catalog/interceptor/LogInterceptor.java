1d5736ff7e067c435730612e7cec10e906cfe288
Tedy Tri Saputro <detik19@gmail.com>
2023-01-01 08:39:02 +0700

    [UPDATE] update java17

--- src/main/java/com/subrutin/catalog/interceptor/LogInterceptor.java
+++ src/main/java/com/subrutin/catalog/interceptor/LogInterceptor.java
@@ -1,11 +1,10 @@
 package com.subrutin.catalog.interceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
