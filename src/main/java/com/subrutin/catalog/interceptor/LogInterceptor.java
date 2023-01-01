8b5ec002eb8bca129cd5b79726556ff9ba3973ff
Tedy Tri Saputro <detik19@gmail.com>
2023-01-01 07:45:26 +0700

    [ADD] add interceptor

---
+++ src/main/java/com/subrutin/catalog/interceptor/LogInterceptor.java
@@ -0,0 +1,35 @@
+package com.subrutin.catalog.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.info("prehandle log interceptor");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		log.info("posthandle log interceptor");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		log.info("aftercompletion log interceptor");
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+}
