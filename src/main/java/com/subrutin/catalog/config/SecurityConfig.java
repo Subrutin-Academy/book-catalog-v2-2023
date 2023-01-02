6217c4ba11c389d6aa90682678b0550fccfdee95
Tedy Tri Saputro <detik19@gmail.com>
2023-01-02 18:35:47 +0700

    [ADD} add jwt token factory

--- src/main/java/com/subrutin/catalog/config/SecurityConfig.java
+++ src/main/java/com/subrutin/catalog/config/SecurityConfig.java
@@ -3,15 +3,26 @@ package com.subrutin.catalog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.subrutin.catalog.security.filter.UsernamePasswordAuthProcessingFilter;
+import com.subrutin.catalog.security.handler.UsernamePasswordAuthFailureHandler;
+import com.subrutin.catalog.security.handler.UsernamePasswordAuthSucessHandler;
+import com.subrutin.catalog.security.provider.UsernamePasswordAuthProvider;
+import com.subrutin.catalog.security.util.JWTTokenFactory;
 import com.subrutin.catalog.service.AppUserService;
 
 @EnableWebSecurity
@@ -19,16 +30,42 @@ import com.subrutin.catalog.service.AppUserService;
 @Configuration
 public class SecurityConfig {
 	
-	@Autowired
-	private AppUserService appUserService;
+	private final static String AUTH_URL = "/v1/login";
+	
 	
 	@Autowired
-	void registerProvider(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
-		auth.userDetailsService(appUserService).passwordEncoder(passwordEncoder);
+	private UsernamePasswordAuthProvider usernamePasswordAuthProvider;
+	
+	@Bean
+	public AuthenticationSuccessHandler successHandler(ObjectMapper objectMapper, JWTTokenFactory factory) {
+		return new UsernamePasswordAuthSucessHandler(objectMapper, factory);
 	}
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public AuthenticationFailureHandler failureHandler(ObjectMapper objectMapper) {
+		return new UsernamePasswordAuthFailureHandler(objectMapper);
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	public UsernamePasswordAuthProcessingFilter usernamePasswordAuthProcessingFilter(ObjectMapper objectMapper, 
+			AuthenticationSuccessHandler successHandler, AuthenticationFailureHandler failureHandler, AuthenticationManager authManager ) {
+		UsernamePasswordAuthProcessingFilter filter = new UsernamePasswordAuthProcessingFilter(AUTH_URL, objectMapper, successHandler, failureHandler);
+		filter.setAuthenticationManager(authManager);
+		return filter;
+	}
+	
+	@Autowired
+	void registerProvider(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(usernamePasswordAuthProvider);
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, UsernamePasswordAuthProcessingFilter usernamePasswordAuthProcessingFilter) throws Exception {
 		http.authorizeHttpRequests().anyRequest().authenticated()
 		.and()
 		.csrf().disable()
@@ -36,6 +73,7 @@ public class SecurityConfig {
 		.and()
 		.httpBasic();
 		
+		http.addFilterBefore(usernamePasswordAuthProcessingFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
